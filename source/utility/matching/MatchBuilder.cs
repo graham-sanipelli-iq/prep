31c258b05108ee09dd1eeba9d6c742666a95380b
REPLACE WITH YOUR NAME <REPLACE WITH YOUR EMAIL ADDRESS>
2017-03-20 15:50:01 -0500

    Pushing new changes

--- source/utility/matching/MatchBuilder.cs
+++ source/utility/matching/MatchBuilder.cs
@@ -2,23 +2,32 @@
 
 namespace code.utility.matching
 {
-  public class MatchBuilder<ItemToMatch, Property>
-  {
-    PropertyAccessor<ItemToMatch, Property> accessor;
-
-    public MatchBuilder(PropertyAccessor<ItemToMatch, Property> property_accessor)
+    public class MatchBuilder<ItemToMatch, Property>
     {
-      accessor = property_accessor;
-    }
+        PropertyAccessor<ItemToMatch, Property> accessor;
 
-    public IMatchA<ItemToMatch> equal_to(Property property)
-    {
-      return new CriteriaMatch<ItemToMatch>(item => accessor(item).Equals(property));
-    }
+        public MatchBuilder(PropertyAccessor<ItemToMatch, Property> property_accessor)
+        {
+            accessor = property_accessor;
+        }
 
-    public IMatchA<ItemToMatch> equal_to_any(params Property[] values)
-    {
-      throw new NotImplementedException();
+        public IMatchA<ItemToMatch> equal_to(Property property)
+        {
+            return new CriteriaMatch<ItemToMatch>(item => accessor(item).Equals(property));
+        }
+
+        public IMatchA<ItemToMatch> equal_to_any(params Property[] values)
+        {
+            return new CriteriaMatch<ItemToMatch>(item =>
+            {
+                foreach (var value in values)
+                {
+                    if (accessor(item).Equals(value))
+                        return true;
+                }
+
+                return false;
+            });
+        }
     }
-  }
 }

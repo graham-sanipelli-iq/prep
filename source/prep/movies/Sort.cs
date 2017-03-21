9936539d631753e30d90c6f5af4156c60335bec4
REPLACE WITH YOUR NAME <REPLACE WITH YOUR EMAIL ADDRESS>
2017-03-21 13:43:09 -0500

    Pushing new changes

---
+++ source/prep/movies/Sort.cs
@@ -0,0 +1,14 @@
+﻿using code.utility;
+using code.utility.matching;
+using System;
+
+namespace code.prep.movies
+{
+  public class Sort<ItemToMatch>
+  {
+    public static ICompareTwoItems<PropertyType> by_descending<out PropertyType>(IGetTheValueOfAProperty<ItemToMatch, PropertyType> accessor) where PropertyType : IComparable<PropertyType>
+    {
+      return (a, b) => a.CompareTo(b);
+    }
+  }
+}
